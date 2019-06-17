8e6897ce371f134c5a67d90016b31e4ea23d9fd7
juga <juga@sequoia-pgp.org>
2019-06-17 13:58:32 +0000

    fixup! tool: Add wkd generate subcommand

--- tool/src/sq.rs
+++ tool/src/sq.rs
@@ -502,7 +502,9 @@ fn real_main() -> Result<(), failure::Error> {
                     let parser = TPKParser::from_bytes(&buffer)?;
                     let tpks: Vec<TPK> = parser.filter_map(|tpk| tpk.ok())
                         .collect();
-                    wkd::generate(domain, &tpks, &base_path, direct_method)?;
+                    wkd::generate(domain, &tpks, &base_path, direct_method)
+                        .context(format!("Failed to generate the WKD in {}.",
+                                         base_path))?;
                 }
                 _ => unreachable!(),
             }
