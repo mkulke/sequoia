0b38b2e691c7376d762f07c529e8b3d0b8b7ecc6
Justus Winter <justus@sequoia-pgp.org>
2020-01-21 12:07:03 +0100

    openpgp: Add some accessors to KeyAmalgamation, improve docs.

--- openpgp/src/cert/key_amalgamation.rs
+++ openpgp/src/cert/key_amalgamation.rs
@@ -239,7 +239,8 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
                 binding: KeyAmalgamationBinding::Primary(),
                 ..
             } =>
-                self.cert.primary_key_signature(self.time)
+                self.cert.userids().primary(self.time())
+                    .and_then(|p| p.binding_signature())
                     .ok_or_else(|| Error::NoBindingSignature(self.time))?,
             KeyAmalgamation {
                 binding: KeyAmalgamationBinding::Subordinate(binding),
@@ -258,6 +259,15 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
         self.binding_signature
     }
 
+    /// Returns the Certificate's direct key signature as of the
+    /// reference time, if any.
+    ///
+    /// Subkeys on direct key signatures apply to all components of
+    /// the certificate.
+    pub fn direct_key_signature(&self) -> Option<&'a Signature> {
+        self.cert.primary.binding_signature(self.time())
+    }
+
     /// Returns the key's revocation status as of the amalgamation's
     /// reference time.
     ///
@@ -266,7 +276,8 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
     {
         match self {
             KeyAmalgamation { binding: KeyAmalgamationBinding::Primary(), .. } =>
-                self.cert.revoked(self.time()),
+                self.cert.primary._revoked(true, Some(self.binding_signature()),
+                                           self.time()),
             KeyAmalgamation { binding: KeyAmalgamationBinding::Subordinate(ref binding), .. } =>
                 binding.revoked(self.time()),
         }
@@ -281,12 +292,24 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
 
     /// Returns the key's key flags as of the amalgamtion's
     /// reference time.
+    ///
+    /// Considers both the binding signature and the direct key
+    /// signature.  Information in the binding signature takes
+    /// precedence over the direct key signature.  See also [Section
+    /// 5.2.3.3 of RFC 4880].
+    ///
+    ///   [Section 5.2.3.3 of RFC 4880]: https://tools.ietf.org/html/rfc4880#section-5.2.3.3
     pub fn key_flags(&self) -> Option<KeyFlags> {
-        self.binding_signature.key_flags()
+        self.binding_signature().key_flags()
+            .or_else(|| self.direct_key_signature()
+                     .and_then(|sig| sig.key_flags()))
     }
 
     /// Returns whether the key has at least one of the specified key
     /// flags as of the amalgamtion's reference time.
+    ///
+    /// Key flags are computed as described in
+    /// [`key_flags()`](#method.key_flags).
     pub fn has_any_key_flag<F>(&self, flags: F) -> bool
         where F: Borrow<KeyFlags>
     {
@@ -296,18 +319,27 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
 
     /// Returns whether key is certification capable as of the
     /// amalgamtion's reference time.
+    ///
+    /// Key flags are computed as described in
+    /// [`key_flags()`](#method.key_flags).
     pub fn for_certification(&self) -> bool {
         self.has_any_key_flag(KeyFlags::empty().set_certification(true))
     }
 
     /// Returns whether key is signing capable as of the amalgamtion's
     /// reference time.
+    ///
+    /// Key flags are computed as described in
+    /// [`key_flags()`](#method.key_flags).
     pub fn for_signing(&self) -> bool {
         self.has_any_key_flag(KeyFlags::empty().set_signing(true))
     }
 
     /// Returns whether key is authentication capable as of the
     /// amalgamtion's reference time.
+    ///
+    /// Key flags are computed as described in
+    /// [`key_flags()`](#method.key_flags).
     pub fn for_authentication(&self) -> bool
     {
         self.has_any_key_flag(KeyFlags::empty().set_authentication(true))
@@ -315,6 +347,9 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
 
     /// Returns whether key is intended for storage encryption as of
     /// the amalgamtion's reference time.
+    ///
+    /// Key flags are computed as described in
+    /// [`key_flags()`](#method.key_flags).
     pub fn for_storage_encryption(&self) -> bool
     {
         self.has_any_key_flag(KeyFlags::empty().set_storage_encryption(true))
@@ -322,6 +357,9 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
 
     /// Returns whether key is intended for transport encryption as of the
     /// amalgamtion's reference time.
+    ///
+    /// Key flags are computed as described in
+    /// [`key_flags()`](#method.key_flags).
     pub fn for_transport_encryption(&self) -> bool
     {
         self.has_any_key_flag(KeyFlags::empty().set_transport_encryption(true))
@@ -329,6 +367,9 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
 
     /// Returns whether the certificateis alive as of the
     /// amalgamtion's reference time.
+    ///
+    /// Key flags are computed as described in
+    /// [`key_flags()`](#method.key_flags).
     pub fn cert_alive(&self) -> Result<()>
     {
         self.cert().alive(self.time())
@@ -338,9 +379,43 @@ impl<'a, P: 'a + key::KeyParts> KeyAmalgamation<'a, P> {
     /// reference time.
     ///
     /// Note: this does not return whether the certificate is valid.
+    ///
+    /// Considers both the binding signature and the direct key
+    /// signature.  Information in the binding signature takes
+    /// precedence over the direct key signature.  See also [Section
+    /// 5.2.3.3 of RFC 4880].
+    ///
+    ///   [Section 5.2.3.3 of RFC 4880]: https://tools.ietf.org/html/rfc4880#section-5.2.3.3
     pub fn alive(&self) -> Result<()>
     {
-        self.binding_signature.key_alive(self.generic_key(), self.time())
+        let sig = {
+            let binding = self.binding_signature();
+            if binding.key_expiration_time().is_some() {
+                Some(binding)
+            } else {
+                self.direct_key_signature()
+            }
+        };
+        if let Some(sig) = sig {
+            sig.key_alive(self.generic_key(), self.time())
+        } else {
+            Err(Error::NoBindingSignature(self.time()).into())
+        }
+    }
+
+    /// Returns the key's expiration time as of the amalgamtion's
+    /// reference time.
+    ///
+    /// Considers both the binding signature and the direct key
+    /// signature.  Information in the binding signature takes
+    /// precedence over the direct key signature.  See also [Section
+    /// 5.2.3.3 of RFC 4880].
+    ///
+    ///   [Section 5.2.3.3 of RFC 4880]: https://tools.ietf.org/html/rfc4880#section-5.2.3.3
+    pub fn key_expiration_time(&self) -> Option<std::time::Duration> {
+        self.binding_signature().key_expiration_time()
+            .or_else(|| self.direct_key_signature()
+                     .and_then(|sig| sig.key_expiration_time()))
     }
 
     /// Returns whether the key contains secret key material.
