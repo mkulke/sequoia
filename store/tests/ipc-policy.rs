ac53e32ffb1f3fa47160f4ce5a8a85f0ecfcbbfe
Justus Winter <justus@sequoia-pgp.org>
2020-12-02 18:16:24 +0100

    store: Port integration tests to the async interface.

--- store/tests/ipc-policy.rs
+++ store/tests/ipc-policy.rs
@@ -5,45 +5,69 @@ use std::env::current_exe;
 use std::path::PathBuf;
 
 use sequoia_core::{Context, NetworkPolicy, IPCPolicy};
-use sequoia_store::{Mapping, REALM_CONTACTS};
+use sequoia_store::{Result, not_sync::Mapping, REALM_CONTACTS};
 
 #[test]
-fn ipc_policy_external() {
-    let ctx = Context::configure()
-        .ephemeral()
-        .lib(current_exe().unwrap().parent().unwrap().parent().unwrap())
-        .network_policy(NetworkPolicy::Offline)
-        .ipc_policy(IPCPolicy::External)
-        .build().unwrap();
-    Mapping::open(&ctx, REALM_CONTACTS, "default").unwrap();
+fn ipc_policy_external() -> Result<()> {
+    async fn f() -> Result<()> {
+        let ctx = Context::configure()
+            .ephemeral()
+            .lib(current_exe().unwrap().parent().unwrap().parent().unwrap())
+            .network_policy(NetworkPolicy::Offline)
+            .ipc_policy(IPCPolicy::External)
+            .build()?;
+        Mapping::open(&ctx, REALM_CONTACTS, "default").await?;
+        Ok(())
+    }
+
+    let mut rt = tokio::runtime::Builder::new()
+        .basic_scheduler().enable_io().enable_time()
+        .build()?;
+    tokio::task::LocalSet::new().block_on(&mut rt, f())
 }
 
 #[test]
-fn ipc_policy_internal() {
-    let ctx = Context::configure()
-        .ephemeral()
-        .lib(PathBuf::from("/i/do/not/exist"))
-        .network_policy(NetworkPolicy::Offline)
-        .ipc_policy(IPCPolicy::Internal)
-        .build().unwrap();
-    Mapping::open(&ctx, REALM_CONTACTS, "default").unwrap();
+fn ipc_policy_internal() -> Result<()> {
+    async fn f() -> Result<()> {
+        let ctx = Context::configure()
+            .ephemeral()
+            .lib(PathBuf::from("/i/do/not/exist"))
+            .network_policy(NetworkPolicy::Offline)
+            .ipc_policy(IPCPolicy::Internal)
+            .build()?;
+        Mapping::open(&ctx, REALM_CONTACTS, "default").await?;
+        Ok(())
+    }
+
+    let mut rt = tokio::runtime::Builder::new()
+        .basic_scheduler().enable_io().enable_time()
+        .build()?;
+    tokio::task::LocalSet::new().block_on(&mut rt, f())
 }
 
 #[test]
-fn ipc_policy_robust() {
-    let ctx = Context::configure()
-        .ephemeral()
-        .lib(current_exe().unwrap().parent().unwrap().parent().unwrap())
-        .network_policy(NetworkPolicy::Offline)
-        .ipc_policy(IPCPolicy::Robust)
-        .build().unwrap();
-    Mapping::open(&ctx, REALM_CONTACTS, "default").unwrap();
+fn ipc_policy_robust() -> Result<()> {
+    async fn f() -> Result<()> {
+        let ctx = Context::configure()
+            .ephemeral()
+            .lib(current_exe().unwrap().parent().unwrap().parent().unwrap())
+            .network_policy(NetworkPolicy::Offline)
+            .ipc_policy(IPCPolicy::Robust)
+            .build()?;
+        Mapping::open(&ctx, REALM_CONTACTS, "default").await?;
 
-    let ctx = Context::configure()
-        .ephemeral()
-        .lib(PathBuf::from("/i/do/not/exist"))
-        .network_policy(NetworkPolicy::Offline)
-        .ipc_policy(IPCPolicy::Robust)
-        .build().unwrap();
-    Mapping::open(&ctx, REALM_CONTACTS, "default").unwrap();
+        let ctx = Context::configure()
+            .ephemeral()
+            .lib(PathBuf::from("/i/do/not/exist"))
+            .network_policy(NetworkPolicy::Offline)
+            .ipc_policy(IPCPolicy::Robust)
+            .build()?;
+        Mapping::open(&ctx, REALM_CONTACTS, "default").await?;
+        Ok(())
+    }
+
+    let mut rt = tokio::runtime::Builder::new()
+        .basic_scheduler().enable_io().enable_time()
+        .build()?;
+    tokio::task::LocalSet::new().block_on(&mut rt, f())
 }
