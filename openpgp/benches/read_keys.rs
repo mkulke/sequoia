1bc8577c27fd036886225826a172c08a903b5f0f
Nora Widdecke <nora@sequoia-pgp.org>
2021-01-18 18:04:30 +0100

    openpgp: Use actual small key.

--- openpgp/benches/read_keys.rs
+++ openpgp/benches/read_keys.rs
@@ -4,19 +4,9 @@ use sequoia_openpgp::cert::Cert;
 use sequoia_openpgp::parse::Parse;
 
 fn read_key(bytes: &[u8]) {
+    // Parse the cert, ignore any errors
     let _ = Cert::from_bytes(bytes);
 }
-
-//fn bench_random(c: &mut Criterion) {
-//    let mut group = c.benchmark_group("bench_crypto_random");
-//
-//    group.sample_size(50);
-//    for i in (8..33).step_by(8) {
-//        group.throughput(Throughput::Bytes(i as u64));
-//        group.bench_function(format!("crypto::random {:02}", i), |b| b.iter(|| run_random(black_box(i)) ));
-//    };
-//    group.finish();
-//}
 
 macro_rules! bench_parse_key {
     ( $filename: expr, $group: expr ) => {
@@ -28,7 +18,7 @@ macro_rules! bench_parse_key {
 fn bench_read_keys(c: &mut Criterion) {
     let mut group = c.benchmark_group("read keys");
     bench_parse_key!("dkg.gpg", group);
-    bench_parse_key!("lutz.gpg", group);
+    bench_parse_key!("neal.pgp", group);
     group.finish();
 }
 
