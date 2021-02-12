c113ea86b6662850651949f7be3b6dcec3d97b47
Nora Widdecke <nora@sequoia-pgp.org>
2021-02-12 17:50:15 +0100

    bench: Add benchmark generate_certs.

---
+++ openpgp/benches/parse_cert.rs
@@ -0,0 +1,30 @@
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+
+use sequoia_openpgp::cert::Cert;
+use sequoia_openpgp::parse::Parse;
+
+use std::convert::TryInto;
+
+fn read_cert(bytes: &[u8]) {
+    // Parse the cert, ignore any errors
+    let _ = Cert::from_bytes(bytes);
+}
+
+macro_rules! bench_parse_cert {
+    ( $filename: expr, $group: expr ) => {
+        let bytes = include_bytes!(concat!("../tests/data/keys/", $filename));
+        $group.throughput(Throughput::Bytes(bytes.len().try_into().unwrap()));
+        $group.bench_function($filename, |b| b.iter(|| read_cert(bytes)));
+    };
+}
+fn bench_parse_certs(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse flooded cert");
+    bench_parse_cert!("dkg.gpg", group);
+    group.finish();
+    let mut group = c.benchmark_group("parse typical cert");
+    bench_parse_cert!("neal.pgp", group);
+    group.finish();
+}
+
+criterion_group!(benches, bench_parse_certs);
+criterion_main!(benches);
