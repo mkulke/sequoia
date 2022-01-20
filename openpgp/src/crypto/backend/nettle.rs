242c36ac97475e3988ed13abcd7e638783542762
Justus Winter <justus@sequoia-pgp.org>
2022-01-20 15:42:30 +0100

    openpgp: Add key derivation function. XXX: currently nettle only

--- openpgp/src/crypto/backend/nettle.rs
+++ openpgp/src/crypto/backend/nettle.rs
@@ -4,6 +4,8 @@ use crate::types::*;
 
 use nettle::random::{Random, Yarrow};
 
+use crate::crypto::SessionKey;
+
 pub mod aead;
 pub mod asymmetric;
 pub mod ecdh;
@@ -20,6 +22,33 @@ pub fn backend() -> String {
 /// Fills the given buffer with random data.
 pub fn random(buf: &mut [u8]) {
     Yarrow::default().random(buf);
+}
+
+/// HKDF instantiated with SHA256.
+///
+/// Used to derive message keys from session keys, and key
+/// encapsulating keys from S2K mechanisms.  In both cases, using a
+/// KDF that includes algorithm information in the given `info`
+/// provides key space separation between cipher algorithms and modes.
+///
+/// `salt`, if given, SHOULD be 32 bytes of salt matching the digest
+/// size of the hash function.  If it is not give, 32 zeros are used
+/// instead.
+///
+/// `okm` must not be larger than 255 * 32 (the size of the hash
+/// digest).
+pub fn hkdf_sha256(ikm: &SessionKey, salt: Option<&[u8]>, info: &[u8],
+                   okm: &mut SessionKey)
+{
+    use nettle::{
+        kdf::hkdf,
+        hash::Sha256,
+    };
+
+    assert!(okm.len() <= 255 * 32);
+    const NO_SALT: [u8; 32] = [0; 32];
+    let salt = salt.unwrap_or(&NO_SALT);
+    hkdf::<Sha256>(&ikm[..], salt, info, okm);
 }
 
 impl PublicKeyAlgorithm {
