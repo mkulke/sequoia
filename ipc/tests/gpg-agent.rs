4a0e33ca09d2b6aa9f315870c9f357f7691382a6
Justus Winter <justus@sequoia-pgp.org>
2022-06-16 13:13:47 +0200

    xxx: add a failing test from previous runs

--- ipc/tests/gpg-agent.rs
+++ ipc/tests/gpg-agent.rs
@@ -215,6 +215,137 @@ fn sign() -> openpgp::Result<()> {
     Ok(())
 }
 
+const BAD_SIGN_KEY: &str = "-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+xVgEYqsJ8xYJKwYBBAHaRw8BAQdAfUB/E4q/4KXVCYOwdx/KRJ5dfUzuWhKQS92F
+uIaVMuEAAQCBqKT9nDUo6VcdJkU9euHtJnVyzK8r9RVZDpFpKFHr5Q94wsALBB8W
+CgB9BYJiqwnzAwsJBwkQj3PPCo6za29HFAAAAAAAHgAgc2FsdEBub3RhdGlvbnMu
+c2VxdW9pYS1wZ3Aub3JnsWbhoMETNIFiPfRvM64Q1+g7Cpjf/w/QT5cVSrq1cW4D
+FQoIApsBAh4BFiEEbr3AxOjW2vy6BFtuj3PPCo6za28AAFVOAP0ZVd7jchLC7P9d
++Cu2rUJ3sDWSmlZdDqFAYRIP87Ka2AEAlDYC/9ooEC+NhG00xpO8EvGxpq0S9frJ
+aFkUqfBOaQ/NE3NvbWVvbmVAZXhhbXBsZS5vcmfCwA4EExYKAIAFgmKrCfMDCwkH
+CRCPc88KjrNrb0cUAAAAAAAeACBzYWx0QG5vdGF0aW9ucy5zZXF1b2lhLXBncC5v
+cmdOTl3Tzas+nFXSEQCscDTYrb02k5nGyUkxJB0OYhHVUwMVCggCmQECmwECHgEW
+IQRuvcDE6Nba/LoEW26Pc88KjrNrbwAAOwQA/3JJh5DP5HBY2yi1pNWvw45oqDl5
+GH6BJWieDbevaGagAQDcRo9xM8d44PRIxbl/RiKAtVll9LufuJ0aRRmdxQaqCsdY
+BGKrCfMWCSsGAQQB2kcPAQEHQMRGCST173LcHXGM4rL4jc7GrT1ohOlp8bSAnt9r
+bCAEAAEAxcO+aAGmeAj5oX5oI4XWP/PdkopaXzZpP+I8Efj2lgMQTMLAvwQYFgoB
+MQWCYqsJ8wkQj3PPCo6za29HFAAAAAAAHgAgc2FsdEBub3RhdGlvbnMuc2VxdW9p
+YS1wZ3Aub3Jno3ks/10/XSnOV+cQlINOFse1UbCdEosHuo4nDb5HOZoCmwK+oAQZ
+FgoAbwWCYqsJ8wkQKcPDPpLCO3FHFAAAAAAAHgAgc2FsdEBub3RhdGlvbnMuc2Vx
+dW9pYS1wZ3Aub3JnsMBBdQlPZkZCAi9BDWTSpnxVL5wQSxVdkMjLoJw9RVEWIQQS
+PtMJ9bs65OYxKoYpw8M+ksI7cQAA3bQBAMHk5D+pg89YiB+AxNhYz1218zRViZUX
+5fQjGcal6s1GAQCP0pmojI6vMXiWaOo1ATGxloqvoWj3V+qA5RtPbxmKBRYhBG69
+wMTo1tr8ugRbbo9zzwqOs2tvAADgzgEA22TQn3B2cc5hKL/oIrCz1Dp1FkF4iGkI
+y0KLnG75ZUMA/RcO6tgrtzHBGpXwHN2U9S+z4qtdqN6sPHBJ9rhVRLwD
+=zzAE
+-----END PGP PRIVATE KEY BLOCK-----
+";
+
+#[test]
+fn bad_sign_key() -> openpgp::Result<()> {
+    use openpgp::policy::StandardPolicy as P;
+
+    let p = &P::new();
+    let ctx = make_context!();
+
+    let cert = openpgp::Cert::from_bytes(BAD_SIGN_KEY)?;
+
+    {
+        let mut buf = Vec::new();
+        cert.as_tsk().serialize(&mut buf).unwrap();
+        gpg_import(&ctx, &buf)?;
+
+        let keypair = KeyPair::new(
+            &ctx,
+            cert.keys().with_policy(p, None).alive().revoked(false)
+                .for_signing().take(1).next().unwrap().key())
+            .unwrap();
+
+        let mut message = Vec::new();
+        {
+            // Start streaming an OpenPGP message.
+            let message = Message::new(&mut message);
+
+            // We want to sign a literal data packet.
+            let signer = Signer::new(message, keypair)
+                 // XXX: Is this necessary?  If so, it shouldn't.
+                .hash_algo(HashAlgorithm::SHA512).unwrap()
+                .build().unwrap();
+
+            // Emit a literal data packet.
+            let mut literal_writer = LiteralWriter::new(
+                signer).build().unwrap();
+
+            // Sign the data.
+            literal_writer.write_all(MESSAGE.as_bytes()).unwrap();
+
+            // Finalize the OpenPGP message to make sure that all data is
+            // written.
+            literal_writer.finalize().unwrap();
+        }
+
+        // Make a helper that that feeds the sender's public key to the
+        // verifier.
+        let helper = Helper { cert: &cert };
+
+        // Now, create a verifier with a helper using the given Certs.
+        let mut verifier = VerifierBuilder::from_bytes(&message)?
+            .with_policy(p, None, helper)?;
+
+        // Verify the data.
+        let mut sink = Vec::new();
+        io::copy(&mut verifier, &mut sink).unwrap();
+        assert_eq!(MESSAGE.as_bytes(), &sink[..]);
+    }
+
+    struct Helper<'a> {
+        cert: &'a openpgp::Cert,
+    }
+
+    impl<'a> VerificationHelper for Helper<'a> {
+        fn get_certs(&mut self, _ids: &[openpgp::KeyHandle])
+                           -> openpgp::Result<Vec<openpgp::Cert>> {
+            // Return public keys for signature verification here.
+            Ok(vec![self.cert.clone()])
+        }
+
+        fn check(&mut self, structure: MessageStructure)
+                 -> openpgp::Result<()> {
+            // In this function, we implement our signature verification
+            // policy.
+
+            let mut good = false;
+            for (i, layer) in structure.into_iter().enumerate() {
+                match (i, layer) {
+                    // First, we are interested in signatures over the
+                    // data, i.e. level 0 signatures.
+                    (0, MessageLayer::SignatureGroup { results }) => {
+                        // Finally, given a VerificationResult, which only says
+                        // whether the signature checks out mathematically, we apply
+                        // our policy.
+                        match results.into_iter().next() {
+                            Some(Ok(_)) => good = true,
+                            Some(Err(e)) =>
+                                return Err(openpgp::Error::from(e).into()),
+                            None => (),
+                        }
+                    },
+                    _ => return Err(anyhow::anyhow!(
+                        "Unexpected message structure")),
+                }
+            }
+
+            if good {
+                Ok(()) // Good signature.
+            } else {
+                Err(anyhow::anyhow!("Signature verification failed"))
+            }
+        }
+    }
+    Ok(())
+}
+
 #[test]
 fn decrypt() -> openpgp::Result<()> {
     use self::CipherSuite::*;
