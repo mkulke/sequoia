64a3ef270b83ad92231551d7e7ae2637efa2bcf5
Justus Winter <justus@sequoia-pgp.org>
2023-05-09 16:29:57 +0200

    xxx not sure where this goes

--- openpgp/src/serialize/stream.rs
+++ openpgp/src/serialize/stream.rs
@@ -3716,6 +3716,7 @@ mod test {
 
         let (tsk, _) = CertBuilder::new()
             .set_cipher_suite(CipherSuite::Cv25519)
+            .set_version(6)?
             .add_transport_encryption_subkey()
             .generate().unwrap();
 
