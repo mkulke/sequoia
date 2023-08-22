ee37f1d7ff2747e799064e07f859f30942ec8e06
Justus Winter <justus@sequoia-pgp.org>
2023-08-22 10:08:49 +0200

    openpgp: Add the SEIPDv2 feature.

--- openpgp/src/packet/signature/subpacket.rs
+++ openpgp/src/packet/signature/subpacket.rs
@@ -7371,8 +7371,7 @@ fn accessors() {
         sig.clone().sign_hash(&mut keypair, hash.clone()).unwrap();
     assert_eq!(sig_.features().unwrap(), feats);
 
-    #[allow(deprecated)]
-    let feats = Features::empty().set_aead();
+    let feats = Features::empty().set_seipdv2();
     sig = sig.set_features(feats.clone()).unwrap();
     let sig_ =
         sig.clone().sign_hash(&mut keypair, hash.clone()).unwrap();
