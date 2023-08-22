c9a1f0530205e5aa2bb19b0876415f84da75708d
Justus Winter <justus@sequoia-pgp.org>
2023-08-22 10:18:57 +0200

    openpgp: Reserve signature subpacket tag 38 ("Key Block").

--- openpgp/src/packet/signature/subpacket.rs
+++ openpgp/src/packet/signature/subpacket.rs
@@ -398,7 +398,7 @@ impl From<u8> for SubpacketTag {
             35 => SubpacketTag::IntendedRecipient,
             37 => SubpacketTag::AttestedCertifications,
             39 => SubpacketTag::PreferredAEADCiphersuites,
-            0| 1| 8| 13| 14| 15| 17| 18| 19 => SubpacketTag::Reserved(u),
+            0| 1| 8| 13| 14| 15| 17| 18| 19 | 38 => SubpacketTag::Reserved(u),
             100..=110 => SubpacketTag::Private(u),
             _ => SubpacketTag::Unknown(u),
         }
@@ -513,7 +513,8 @@ mod tests {
                 SubpacketTag::Reserved(u) =>
                     (u == 0 || u == 1 || u == 8
                      || u == 13 || u == 14 || u == 15
-                     || u == 17 || u == 18 || u == 19),
+                     || u == 17 || u == 18 || u == 19
+                     || u == 39),
                 SubpacketTag::Private(u) => (100..=110).contains(&u),
                 SubpacketTag::Unknown(u) => (u > 33 && u < 100) || u > 110,
                 _ => true
